abc044da1350ec14bf68ebf0d1c340bffa0c3f26
mira_arar <mira.ar230@gmail.com>
2020-08-30 19:12:33 +0300

    Updates

---
+++ precache-manifest.2c539400605c2c59011f584feaa76d85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fd39dafd6e469dae92e7ce8991f36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16b838fb3f335875e590",
+    "url": "/static/js/2.9abd6d30.chunk.js"
+  },
+  {
+    "revision": "d00feeab4f2cf07dc9b288d1d5a65f6c",
+    "url": "/static/js/2.9abd6d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82bd8b42b95a71b94b5",
+    "url": "/static/js/main.2baaf648.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "1a0586c05fb899335f2d420b571a7e2e",
+    "url": "/static/media/adam-solomon-WHUDOzd5IYU-unsplash.1a0586c0.jpg"
+  },
+  {
+    "revision": "5ce6ceecc722d2c65ecc1dc526f8b9c7",
+    "url": "/static/media/boardProject.5ce6ceec.png"
+  },
+  {
+    "revision": "8a993a5dea30b9eab8d1f05c75fef6bc",
+    "url": "/static/media/christopher-gower-m_HRfLhgABo-unsplash.8a993a5d.jpg"
+  },
+  {
+    "revision": "2f191724de75d9ca3a0f6aea9faec354",
+    "url": "/static/media/e-commerce.2f191724.png"
+  },
+  {
+    "revision": "f105027824abcef5361d3cfbf921fe91",
+    "url": "/static/media/jeremy-thomas-4dpAqfTbvKA-unsplash.f1050278.jpg"
+  },
+  {
+    "revision": "81298ccd8a7cbaf8426bae78c17110c9",
+    "url": "/static/media/movieProject.81298ccd.png"
+  }
+]);
